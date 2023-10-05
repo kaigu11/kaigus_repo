88aa6d40c4698630b57132dc0b8e95fcec118f92
kaigu <117625546+kaigu11@users.noreply.github.com>
2023-10-05 18:06:40 +0300

    leap.py

---
+++ leap.py
@@ -0,0 +1,9 @@
+#KAIGU ANN WANGARI SCT 211-0772/2022
+#leap year check
+
+print("Welcome!\nThis app will help you find out if a year is a a leap year or not.")
+year = int(input("Enter the year of your choice: "))
+if (year%4) == 0:
+    print("This is a leap year")
+else:
+    print("This is not a leap year")
