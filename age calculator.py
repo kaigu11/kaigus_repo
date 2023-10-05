c60a572fb3342b18ad94a796feefced611b510f8
kaigu <117625546+kaigu11@users.noreply.github.com>
2023-10-05 18:03:41 +0300

    agecalculator.py

---
+++ age calculator.py
@@ -0,0 +1,11 @@
+#KAIGU ANN WANGARI SCT211-0772/2022
+#age calculator
+
+birthYear = int(input("Enter your birth Year: "))
+currentYear = 2023
+ageInYears = currentYear-birthYear
+ageInMonths = ageInYears*12
+ageInDays = ageInYears*365.24
+print("Your age in years is:", ageInYears)
+print("Your age in months is:", ageInMonths)
+print("Your age in days is:", ageInDays)
